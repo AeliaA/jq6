24868b6b92f0e8b4dab1e373143157f157c2cc0e
Aelia Abbas <aelia.abbas@cityrdbclab5015.auckland.natcoll.ac.nz>
2015-10-20 19:15:05 +1300

    1

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+// prepend
+$(document).ready(function(){
+
+$(".prepend").prepend("Hello"();
+});
+
+// apppend
+$(document).ready(function(){
+
+$(".append").append("Hello"();
+});
+// after
+$(document).ready(function(){
+
+	$(".after").after("Hello"();
+});
+// before
+$(document).ready(function(){
+
+	$(".before").before("Hello"();
+});
+// remove () removes the selected elements n child ele
+// empty () empties the child elements from the selected parent
+$(document).ready(function(){
+
+$("p").remove(".before, .after");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
